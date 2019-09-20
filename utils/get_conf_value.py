3419115abb3e05091ec8e5724b071855a245b225
4G63r <289280282@qq.com>
2019-09-20 10:37:54 +0800

    update files

--- utils/get_conf_value.py
+++ utils/get_conf_value.py
@@ -13,7 +13,7 @@ def get_test_host():
     test环境host
     :return: https://test-cloud.chanjet.com/app_name/0000/0
     """
-    return r.get_conf_value('test_host', 'addr')
+    return r.get_conf_value('HOST', 'test')
 
 
 def get_inte_host():
@@ -21,7 +21,7 @@ def get_inte_host():
     inte环境host
     :return: https://inte-cloud.chanjet.com/app_name/0000/0
     """
-    return r.get_conf_value('inte_host', 'addr')
+    return r.get_conf_value('HOST', 'inte')
 
 
 def get_host_addr():
@@ -29,14 +29,14 @@ def get_host_addr():
     获取host地址
     :return: https://inte-cloud.chanjet.com/cc/0000/0
     """
-    env_mode = r.get_conf_value('env_mode', 'mode')
-    app_name = r.get_conf_value('app_name', 'name').lower()
+    env_mode = r.get_conf_value('TEST_ENV', 'env')
+    app_name = r.get_conf_value('APP_NAME', 'name').lower()
     if env_mode.upper() == 'TEST':
         return get_test_host().replace('app_name', app_name)
     elif env_mode.upper() == 'INTE':
         return get_inte_host().replace('app_name', app_name)
     else:
-        raise Exception('env_mode配置错误!!!')
+        raise Exception('<TEST_ENV>配置错误!!!')
 
 
 def get_account_info():
@@ -44,8 +44,8 @@ def get_account_info():
     获取用户账号密码
     :return:
     """
-    username = r.get_conf_value('account', 'username')
-    password = r.get_conf_value('account', 'password')
+    username = r.get_conf_value('ACCOUNT', 'username')
+    password = r.get_conf_value('ACCOUNT', 'password')
     return {'username': username, 'password': password}
 
 
@@ -54,4 +54,4 @@ def get_platform():
     获取平台名称
     :return: WEB / APP
     """
-    return r.get_conf_value('platform', 'name').upper()
+    return r.get_conf_value('PLATFORM_NAME', 'name').upper()
