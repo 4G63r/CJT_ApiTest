e6f20ff82e7af13970f65946b9bf3699e52de5a4
4G63r <289280282@qq.com>
2019-09-26 15:34:06 +0800

    update files

---
+++ conf/filePath.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+# @Author: songxiao
+# @Time: 2019-09-26 15:12
+
+"""
+测试用例，测试数据，测试报告，log日志绝对路径
+"""
+
+import os
+import time
+
+cur_dir = os.path.abspath(os.path.dirname(__file__))
+cur_time = time.strftime('%Y%m%d_%H%M%S')
+
+# testdata路径
+testdata_abspath = '{}/testdatas.xlsx'.format(cur_dir.replace('conf', 'testdata'))
+
+# testcase目录
+testcase_abspath = '{}/testcases.xlsx'.format(cur_dir.replace('conf', 'testcase'))
+
+# report.html路径
+report_abspath = '{}/report_{}.html'.format(cur_dir.replace('conf', 'report'), cur_time)
+
+# log路径
+log_abspath = '{}/log_{}.log'.format(cur_dir.replace('conf', 'log'), cur_time)
