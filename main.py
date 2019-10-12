90771d8ee14f743b62af42011345ed68f3bf2d57
宋宵 <songxiaoa@chanjet.com>
2019-10-12 10:12:18 +0800

    update files

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import subprocess
 from conf import filePath
 
 # command = 'pytest'
+# command = 'pytest'
 command = 'pytest -s runTest/caseRunner.py --html={} --self-contained-html'.format(filePath.report_abspath)
 r = subprocess.call(command, shell=True)
 print(r)
